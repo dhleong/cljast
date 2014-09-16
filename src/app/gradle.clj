0fe81f00f74aea344603c27d4e4397bcdc780763
dhleong <falcone88@gmail.com>
2014-09-16 13:19:18 -0400

    Fix macro; experiment with calling dependencies task

--- src/app/gradle.clj
+++ src/app/gradle.clj
@@ -3,6 +3,7 @@
           (org.gradle.tooling.model GradleProject)
           (org.gradle.tooling.model.eclipse EclipseProject)
           (org.gradle.tooling.model.idea IdeaProject)
+          (java.io ByteArrayOutputStream) 
           (java.net URI))
   (:require [clojure.java.io :refer [file]]))
 
@@ -23,26 +24,39 @@
       (.useGradleUserHomeDir conn (file home)))
     (.connect conn)))
 
-(defn gradle-project
-  "Load an EclipseProject instance from Gradle"
-  [& args]
-  (with-gradle args 
-    (.getModel proj-conn EclipseProject)))
-
 (defmacro with-gradle
   "Open a gradle project connection, binding to proj-conn
   by default (you can add `:as name` to your args to customize),
   execute the statements, and close the connection when done"
   [gradle-args & body]
-  (let [args-map (apply assoc {} (drop 1 gradle-args))
-        var-name (get args-map :as 'proj-conn)
-        pass-args (if (contains? args-map :as)
-                    (drop-last 2 gradle-args)
-                    gradle-args)]
-    `(let [~var-name (apply gradle-connection ~(vec pass-args))]
+  (let [args-map (if (seq? gradle-args) 
+                   (apply assoc {} (drop 1 gradle-args))
+                   {})
+        var-name (get args-map :as 'proj-conn)]
+    `(let [~var-name (apply gradle-connection ~gradle-args)]
        (try
          (do ~body)
          (finally (.close ~var-name))))))
+
+(defn gradle-project
+  "Load an EclipseProject instance from Gradle"
+  [& args]
+  (with-gradle args
+    (.getModel proj-conn EclipseProject)))
+
+(defn gradle-list-dependencies
+  "Wacky way of listing dependencies"
+  [& args]
+  (let [out (ByteArrayOutputStream.)] 
+    (with-gradle args
+      (.. proj-conn
+          (newBuild)
+          (forTasks (into-array ["dependencies"]))
+          (setStandardOutput out)
+          (run)
+          ))
+    (.toString out)))
+ 
 
 (defn get-dependencies 
   "List dependency files for an EclipseProject"
