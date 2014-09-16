3f1180573ec2063c872cc476d9583e515f3f0280
dhleong <falcone88@gmail.com>
2014-09-16 11:24:53 -0400

    Add build.gradle for testing
    It appears that the tooling works very well... unless you happen
    to be using the android plugin. Lovely.

--- test/app/gradle_test.clj
+++ test/app/gradle_test.clj
@@ -1,13 +1,18 @@
 (ns app.gradle-test
   (import (org.eclipse.jdt.core.dom AST ASTNode))
   (:require [clojure.test :refer :all]
-            [app.gradle :refer :all]))
+            [app.gradle :refer :all]
+            [clojure.java.io :refer [file]]
+            ))
 
 (def gradle-version "1.11")
+(def gradle-proj-dir "test/app/gradle")
 
 (deftest gradle-project-test
   (testing "classpath"
-    (is (nil? (get-dependencies
-                (gradle-project gradle-proj-dir ))))))
+    (is (not-empty (->> gradle-proj-dir
+                        (gradle-project)
+                        (get-dependencies)
+                        )))))
 
 ;; (clojure.test/run-tests)
